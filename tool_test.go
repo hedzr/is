e665825380c74b67621887d04a2e6c46b4e24978
hz <hedzrz@gmail.com>
2023-11-07 11:22:46 +0800

    fix tty function signatures

--- tool_test.go
+++ tool_test.go
@@ -1,6 +1,7 @@
 package is
 
 import (
+	"os"
 	"runtime/debug"
 	"testing"
 
@@ -15,4 +16,27 @@ func TestDebugBuildInfo(t *testing.T) {
 	if info, ok := debug.ReadBuildInfo(); ok {
 		t.Logf("info: %+v", info)
 	}
+}
+
+func TestTtyFunctions(t *testing.T) {
+	t.Logf(`TTY: %v, colorful: %v
+	string is escaped(plain): %v
+	string is escaped(color): %v
+	`,
+		Tty(os.Stdout), ColoredTty(os.Stdout),
+		TtyEscaped("plain text"),
+		TtyEscaped("\x1b[2mcolor\x1b0m text"),
+	)
+
+	t.Logf("%v", StripEscapes(`
+	
+		<code>code</code>
+	NC Cool
+	 But it's tight.
+	  Hold On!
+	Hurry Up.
+	`))
+
+	x, y := GetTtySize()
+	t.Logf("%v, %v", x, y)
 }
