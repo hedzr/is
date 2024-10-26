0e982edb4b348615fd9db10e0c708a729add0dd4
hz <hedzrz@gmail.com>
2024-10-27 07:13:04 +0800

    update small example app

--- _examples/small/main.go
+++ _examples/small/main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/hedzr/is"
 	"github.com/hedzr/is/basics"
+	"github.com/hedzr/is/term"
 	"github.com/hedzr/is/term/color"
 )
 
@@ -18,15 +19,15 @@ func main() {
 
 	is.RegisterStateGetter("custom", func() bool { return is.InVscodeTerminal() })
 
-	println(is.InTesting())
-	println(is.State("in-testing"))
-	println(is.State("custom")) // detects a state with custom detector
-	println(is.Env().GetDebugLevel())
+	println("state.InTesting:   ", is.InTesting())
+	println("state.in-testing:  ", is.State("in-testing"))
+	println("state.custom:      ", is.State("custom")) // detects a state with custom detector
+	println("env.GetDebugLevel: ", is.Env().GetDebugLevel())
 	if is.InDebugMode() {
 		slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true, Level: slog.LevelDebug})))
 	}
 
-	fmt.Printf("%v", color.GetCPT().Translate(`<code>code</code> | <kbd>CTRL</kbd>
+	fmt.Printf("\n%v", color.GetCPT().Translate(`<code>code</code> | <kbd>CTRL</kbd>
 		<b>bold / strong / em</b>
 		<i>italic / cite</i>
 		<u>underline</u>
@@ -34,6 +35,17 @@ func main() {
 		<del>strike / del </del>
 		<font color="green">green text</font>
 `, color.FgDefault))
+
+	println("term.IsTerminal:               ", term.IsTerminal(int(os.Stdout.Fd())))
+	println("term.IsAnsiEscaped:            ", term.IsAnsiEscaped(color.GetCPT().Translate(`<code>code</code>`, color.FgDefault)))
+	println("term.IsCharDevice(stdout):     ", term.IsCharDevice(os.Stdout))
+	rows, cols, err := term.GetFdSize(os.Stdout.Fd())
+	println("term.GetFdSize(stdout):        ", rows, cols, err)
+	rows, cols, err = term.GetTtySizeByFd(os.Stdout.Fd())
+	println("term.GetTtySizeByFd(stdout):   ", rows, cols, err)
+	rows, cols, err = term.GetTtySizeByFile(os.Stdout)
+	println("term.GetTtySizeByFile(stdout): ", rows, cols, err)
+	println("term.IsStartupByDoubleClick:   ", term.IsStartupByDoubleClick())
 
 	ctx, cancel := context.WithCancel(context.Background())
 	catcher := is.Signals().Catch()
