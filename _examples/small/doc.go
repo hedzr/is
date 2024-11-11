e46ba457213eb06e1037833fd6d5086083465f37
hz <hedzrz@gmail.com>
2024-11-11 09:50:01 +0800

    bump to v0.6.1

--- _examples/small/doc.go
+++ _examples/small/doc.go
@@ -3,4 +3,4 @@ package main
 const AppNameExample = "small" // appName for the current demo app
 
 const appName = "is"     // appName of hedzr/is package
-const version = "v0.6.0" // version of hedzr/is package | update it while bumping hedzr/is' version
+const version = "v0.6.1" // version of hedzr/is package | update it while bumping hedzr/is' version
