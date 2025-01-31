96b4322d3e0d876079bcc7ab3bd79b09b3d55390
hz <hedzrz@gmail.com>
2025-02-01 07:32:01 +0800

    fix is.ToBool

--- term/tobool.go
+++ term/tobool.go
@@ -10,10 +10,32 @@ func ToBool(val any, defaultVal ...bool) (ret bool) {
 		if v, ok := val.(bool); ok {
 			return v
 		}
-		switch val.(type) {
-		case int, int8, int16, int32, int64,
-			uint, uint8, uint16, uint32, uint64:
-			return val != 0
+		// switch v := val.(type) {
+		// case int, int8, int16, int32, int64,
+		// 	uint, uint8, uint16, uint32, uint64:
+		// 	return v != 0
+		// }
+		switch v := val.(type) {
+		case int:
+			return v != 0
+		case int8:
+			return v != 0
+		case int16:
+			return v != 0
+		case int32:
+			return v != 0
+		case int64:
+			return v != 0
+		case uint:
+			return v != 0
+		case uint8:
+			return v != 0
+		case uint16:
+			return v != 0
+		case uint32:
+			return v != 0
+		case uint64:
+			return v != 0
 		}
 		if v, ok := val.(string); ok {
 			return toBool(v, defaultVal...)
