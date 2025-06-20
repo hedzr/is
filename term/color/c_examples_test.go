cc020b2d2876a23ee4b91d37ce34ad16e4e497a0
hz <hedzrz@gmail.com>
2025-06-21 01:47:05 +0800

    added examples for new apis

--- term/color/c_examples_test.go
+++ term/color/c_examples_test.go
@@ -2,6 +2,9 @@ package color_test
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"testing"
 
 	"github.com/hedzr/is/states"
 	"github.com/hedzr/is/term/color"
@@ -215,4 +218,145 @@ func ExampleCursor_StripLeftTabsColorful() {
 	// [7minverse mark[0m[0m
 	// [9mstrike / del [0m[0m
 	// [32mgreen text[0m[0m
+}
+
+func ExampleCSICodes() {
+	c := color.CSICursorUp.Code1(7)
+	fmt.Printf("%s", c) // move cursor up 7 lines
+	d := color.CSICursorDown.Code1(7)
+	fmt.Printf("%s", d) // move cursor down 7 lines
+
+	// move cursor to new position
+
+	fmt.Printf("%s", color.New().SavePos().Build())
+
+	var cx color.CSICodes
+	cx.AddCode2(color.CSICursorPosition, 2, 3) // move cursor to row 2 col 3
+	fmt.Printf("%s", cx)
+
+	fmt.Printf("%s", color.New().RestorePos().Build())
+
+	// Output:
+	// [7A[7B[s[2,3H[u
+}
+
+func ExampleNewStyle() {
+	c := color.NewStyle()
+	c.Add(
+		color.NewColor16(color.FgYellow),    // fg
+		color.NewColor16m(77, 88, 99, true), // bg
+		// color.Reset,
+	)
+	fmt.Printf("%sHello, World!%s\n", c, color.Reset)
+	// Output:
+	// [33m[48;2;77;88;99mHello, World![0m
+}
+
+func ExampleNewRowsBlock() {
+	rb := color.NewRowsBlock()
+
+	// the following outputs will be displayed in first
+	// line of the RowsBlock.
+	for ul := range 10 {
+		spc := strings.Repeat("+", ul)
+		str := fmt.Sprintf("%sHello, World!\n", spc)
+		rb.Update(str)
+	}
+	// Output:
+	// [0G[2KHello, World!
+	// [0G[2K[1A[2K[0G+Hello, World!
+	// [0G[2K[1A[2K[0G++Hello, World!
+	// [0G[2K[1A[2K[0G+++Hello, World!
+	// [0G[2K[1A[2K[0G++++Hello, World!
+	// [0G[2K[1A[2K[0G+++++Hello, World!
+	// [0G[2K[1A[2K[0G++++++Hello, World!
+	// [0G[2K[1A[2K[0G+++++++Hello, World!
+	// [0G[2K[1A[2K[0G++++++++Hello, World!
+	// [0G[2K[1A[2K[0G+++++++++Hello, World!
+}
+
+func ExampleNewSGR() {
+	for i, sgrs := range []struct {
+		pre, post color.CSIsgr
+		desc      string
+	}{
+		{color.SGRbold, color.SGRresetBoldAndDim, "bold"},
+		{color.SGRdim, color.SGRresetBoldAndDim, "dim"},
+		{color.SGRitalic, color.SGRresetItalic, "italic"},
+		{color.SGRunderline, color.SGRresetUnderline, "underline"},
+		{color.SGRslowblink, color.SGRresetSlowBlink, "blink"},
+		{color.SGRrapidblink, color.SGRresetRapidBlink, "fast blink"},
+		{color.SGRinverse, color.SGRresetInverse, "inverse"},
+		{color.SGRhide, color.SGRresetHide, "hide"},
+		{color.SGRstrike, color.SGRresetStrike, "strike"},
+		{color.SGRframed, color.SGRneitherFramedNorEncircled, "framed"},
+		{color.SGRencircled, color.SGRneitherFramedNorEncircled, "encircled"},
+		{color.SGRoverlined, color.SGRnotoverlined, "overlined"},
+		{color.SGRideogramUnderline, color.SGRresetIdeogram, "ideogram underline"},
+		{color.SGRideogramDoubleUnderline, color.SGRresetIdeogram, "ideogram double underline"},
+		{color.SGRideogramOverline, color.SGRresetIdeogram, "ideogram overline"},
+		{color.SGRideogramDoubleOverline, color.SGRresetIdeogram, "ideogram double overline"},
+		{color.SGRideogramStressMarking, color.SGRresetIdeogram, "ideogram stress marking"},
+		{color.SGRsuperscript, color.SGRresetSuperscriptAndSubscript, "superscript"},
+		{color.SGRsubscript, color.SGRresetSuperscriptAndSubscript, "subscript"},
+		// {color.SGRdim, color.SGRresetDim},
+	} {
+		str := fmt.Sprintf(`%5d. %s%s%s %s`,
+			i, sgrs.pre,
+			"Hello, World!",
+			sgrs.post,
+			sgrs.desc,
+		)
+		fmt.Println(str)
+	}
+
+	fmt.Println(color.SGRreset)
+
+	// SGRsetFg
+	fmt.Printf("\x1b[%d;5;9m[ 9 TEST string HERE]%s\n",
+		color.SGRsetFg,
+		color.SGRdefaultFg,
+	)
+
+	fmt.Printf("\x1b[%d;5;21m[21 TEST string HERE]%s\n",
+		color.SGRsetFg,
+		color.SGRdefaultFg,
+	)
+
+	fmt.Println(color.SGRreset)
+
+	// Output:
+	//     0. [1mHello, World![22m bold
+	//     1. [2mHello, World![22m dim
+	//     2. [3mHello, World![23m italic
+	//     3. [4mHello, World![24m underline
+	//     4. [5mHello, World![25m blink
+	//     5. [6mHello, World![26m fast blink
+	//     6. [7mHello, World![27m inverse
+	//     7. [8mHello, World![28m hide
+	//     8. [9mHello, World![29m strike
+	//     9. [51mHello, World![54m framed
+	//    10. [52mHello, World![54m encircled
+	//    11. [53mHello, World![55m overlined
+	//    12. [60mHello, World![65m ideogram underline
+	//    13. [61mHello, World![65m ideogram double underline
+	//    14. [62mHello, World![65m ideogram overline
+	//    15. [63mHello, World![65m ideogram double overline
+	//    16. [64mHello, World![65m ideogram stress marking
+	//    17. [73mHello, World![75m superscript
+	//    18. [74mHello, World![75m subscript
+	// [0m
+	// [38;5;9m[ 9 TEST string HERE][39m
+	// [38;5;21m[21 TEST string HERE][39m
+	// [0m
+}
+func TestExampleNewSGR(t *testing.T) {
+	if !testing.Verbose() {
+		return
+	}
+	ExampleNewSGR()
+}
+
+func TestColor256table(t *testing.T) {
+	color.Color256table(os.Stdout)
 }
