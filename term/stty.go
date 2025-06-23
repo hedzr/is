657d33605d2b55f5181a87268ced272af6cb3c8f
hz <hedzrz@gmail.com>
2025-06-24 07:22:46 +0800

    stty.go: use `chk`

--- term/stty.go
+++ term/stty.go
@@ -23,3 +23,15 @@ func StatStdout() (normalFile, redirected, piped, term bool) { return chk.StatSt
 
 func StatStdoutString() (status string) { return chk.StatStdoutString() }
 func StdoutIsPiped() (b bool)           { return chk.StdoutIsPiped() }
+
+// ReadTill reads a line or a string ending with delim of input
+// from a terminal without local echo.
+func ReadTill(fd int, delim byte) (string, bool, error) {
+	return chk.ReadTill(fd, delim)
+}
+
+// DisableColors return a state which identify wherher needs disable
+// colorful console output always.
+func DisableColors() bool { return chk.DisableColors }
+
+func SetDisableColors(b bool) { chk.DisableColors = b }
