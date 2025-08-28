f1f9edb5a1d55afd536b9c83a82e285e372f65ac
hz <hedzrz@gmail.com>
2025-08-28 08:20:56 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -224,10 +224,6 @@ sleepx() { tip "sleeping..." && (($#)) && \sleep "$@"; }
 
 ######### SIMPLE BASH.SH FOOTER BEGIN #########
 
-# The better consice way to get baseDir, ie. $CD, is:
-#       CD=$(cd `dirname "$0"`;pwd)
-# It will open a sub-shell to print the folder name of the running shell-script.
-
 is_darwin() { [[ $OSTYPE == darwin* ]]; }
 is_darwin_sillicon() { is_darwin && [[ $(uname_mach) == arm64 ]]; }
 is_linux() { [[ $OSTYPE == linux* ]]; }
@@ -244,17 +240,21 @@ is_git_dirty() {
 	fi
 }
 
+###
+# The better consice way to get baseDir, ie. $CD, is:
+#       CD=$(cd `dirname "$0"`;pwd)
+# It will open a sub-shell to print the folder name of the running shell-script.
+
 dbg() { ((DEBUG)) && printf ">>> \e[0;38;2;133;133;133m$@\e[0m\n" || :; }
 tip() { printf "\e[0;38;2;133;133;133m>>> $@\e[0m\n"; }
+wrn() { printf "\e[0;38;2;172;172;22m... [WARN] \e[0;38;2;11;11;11m$@\e[0m\n"; }
 err() { printf "\e[0;33;1;133;133;133m>>> $@\e[0m\n" 1>&2; }
 fn_exists() { LC_ALL=C type $1 2>/dev/null | grep -qE '(shell function)|(a function)'; }
-CD="$(cd $(dirname "$0") && pwd)" && BASH_SH_VERSION=v20241021 && DEBUG=${DEBUG:-0} && PROVISIONING=${PROVISIONING:-0}
-LS_OPT="--color"
-is_darwin && LS_OPT="-G"
+CD="$(cd $(dirname "$0") && pwd)" && BASH_SH_VERSION=v20250808 && DEBUG=${DEBUG:-0} && PROVISIONING=${PROVISIONING:-0}
 (($#)) && {
 	dbg "$# arg(s) | CD = $CD"
 	check_entry() {
-		local prefix="$1" cmd="$2" && shift && shift
+		local prefix="${1:-boot}" cmd="${2:-first}" && shift && shift
 		if fn_exists "${prefix}_${cmd}_entry"; then
 			eval "${prefix}_${cmd}_entry" "$@"
 		elif fn_exists "${cmd}_entry"; then
