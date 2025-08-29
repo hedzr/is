efe0c7a5c452f161593ce09b6ee00fbe4ed5b7a2
hz <hedzrz@gmail.com>
2025-08-30 07:02:35 +0800

    bump to v0.8.55
      - UPGRADE GO TOOLCHAIN TO 1.24.x (1.24.5)
      - `Translator` will keep the leading spaces now.

--- _examples/small/doc.go
+++ _examples/small/doc.go
@@ -3,5 +3,5 @@ package main
 const AppNameExample = "small" // appName for the current demo app
 
 const appName = "is"      // appName of hedzr/is package
-const version = "v0.8.53" // version of hedzr/is package | update it while bumping hedzr/is' version
+const version = "v0.8.55" // version of hedzr/is package | update it while bumping hedzr/is' version
 const Version = version
