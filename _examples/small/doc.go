f146448c862d2f8b6fd202d2644280f99ae22971
hz <hedzrz@gmail.com>
2026-02-15 08:05:41 +0800

    bump to v0.9.0   - upgrade go toolchain to 1.25.0

--- _examples/small/doc.go
+++ _examples/small/doc.go
@@ -3,7 +3,7 @@ package main
 const AppNameExample = "small" // appName for the current demo app
 
 const (
-	appName = "is"      // appName of hedzr/is package
-	version = "v0.8.68" // version of hedzr/is package | update it while bumping hedzr/is' version
+	appName = "is"     // appName of hedzr/is package
+	version = "v0.9.0" // version of hedzr/is package | update it while bumping hedzr/is' version
 	Version = version
 )
