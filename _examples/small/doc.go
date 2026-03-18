555f760653b6d4501f42e71dddb97d04f5836ad3
hz <hedzrz@gmail.com>
2026-03-19 07:56:15 +0800

    bump to v0.9.1   - security patch

--- _examples/small/doc.go
+++ _examples/small/doc.go
@@ -4,6 +4,6 @@ const AppNameExample = "small" // appName for the current demo app
 
 const (
 	appName = "is"     // appName of hedzr/is package
-	version = "v0.9.0" // version of hedzr/is package | update it while bumping hedzr/is' version
+	version = "v0.9.1" // version of hedzr/is package | update it while bumping hedzr/is' version
 	Version = version
 )
